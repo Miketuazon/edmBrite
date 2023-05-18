ea50590d81157af37aa5e87d2f5980fd784c30f8
Miketuazon <michael.d.tuazon@gmail.com>
2023-05-18 17:29:23 -0400

    need to redo models

--- app/api/event_routes.py
+++ app/api/event_routes.py
@@ -3,6 +3,7 @@ from flask_login import login_required
 from app.models import Event
 import os
 import requests
+import random
 
 event_routes = Blueprint('events', __name__)
 
@@ -14,18 +15,23 @@ def get_all_events():
     api_key = os.getenv('EDMTRAIN_KEY')
     url = f"https://edmtrain.com/api/events?client={api_key}"
     edmtrain_response = requests.get(url)
-    data_from_edmtrain = edmtrain_response.json()
+    data_from_edmtrain = edmtrain_response.json()['data']
+
+    # random_events_30 = random.sample(data_from_edmtrain, k=30)
+    # for event in data_from_edmtrain:
+        # print(event)
 
     # commented out for now, getting a TypeError: Object of type Event is not JSON serializable
-    # events = Event.query.all()
-    # return_list = []
-    # for event in events:
-    #     event_dict = event.to_dict()
-    #     return_list.append(event_dict)
+    events = Event.query.all()
+    return_list = []
+    for event in events:
+        event_dict = event.to_dict()
+        breakpoint
+        return_list.append(event_dict)
 
     # breakpoint()
     data = {
-        'edmtrain_events': data_from_edmtrain,
-        # 'db_data': events,
+        # 'edmtrain_events': random_events_30,
+        'db_data': return_list,
     }
     return data
--- react-app/src/components/Events/EventsPage.js
+++ react-app/src/components/Events/EventsPage.js
@@ -1,34 +1,14 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useState } from "react";
 import './EventsPage.css'
-import dotenv from 'dotenv';
 import { getAllEDMTRAINEvents } from "../../store/edmtrain";
 
-dotenv.config();
 
 const EventsPage = () => {
     // Reminder: Need to learn AWS to serve images
-    // const [events, setEvents] = useState([])
     const dispatch = useDispatch()
-        // old way of getting events fetching from front end
-    // const apiKey = process.env.EDMTRAIN_KEY;
-    // console.log(apiKey);
-    // const edmAPI = 'https://edmtrain.com/api'
-    // console.log("edmAPI =>", edmAPI)``
-    // useEffect(() => {
-    //     fetch(`${edmAPI}/events?client=ommittedForGithub`)
-    //         .then(res => res.json())
-    //         .then(data => setEvents(data))
-    //         .catch(err => console.log(err))
-    // }, [])
-
-    // trying out new way from Roosevelt
     const eventsObj = useSelector((state) => state.events)
-    console.log('eventsObj =>', eventsObj)
     const events = Object.values(eventsObj)
-    debugger
-    console.log('events => ', events)
     useEffect(() => {
         dispatch(getAllEDMTRAINEvents())
     }, [dispatch])
@@ -36,10 +16,10 @@ const EventsPage = () => {
     if (!events.length) return <>Loading.....</>
     return (
         <div className="events-page">
-            <header className="events-data-header">
-                <h2>EDMTRAIN Events</h2>
-            </header>
             <ul className="edmtrain-events-list">
+                <header className="events-data-header">
+                    <h2>EDMTRAIN Events</h2>
+                </header>
                 {events?.filter(event => event.name)?.map((event, index) => (
                     <li key={index} className="edmtrain-event">
                         <div className="event-id-and-name">{event.id} | {event.name}</div>
