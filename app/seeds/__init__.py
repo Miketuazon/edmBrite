3b18396f0d9a5059e419f52b4f41193eab338b76
Miketuazon <michael.d.tuazon@gmail.com>
2023-05-18 17:59:10 -0400

    finally fixed models again

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -20,11 +20,11 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
-        undo_events()
-        undo_genres()
-        undo_likes()
         undo_tickets()
+        undo_likes()
+        undo_genres()
+        undo_events()
+        undo_users()
 
     seed_users()
     seed_events()
@@ -37,10 +37,10 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
-    undo_events()
-    undo_genres()
-    undo_likes()
     undo_tickets()
+    undo_likes()
+    undo_genres()
+    undo_events()
+    undo_users()
 
     # Add other undo functions here
--- app/seeds/events.py
+++ app/seeds/events.py
@@ -70,7 +70,7 @@ def seed_events():
 
 def undo_events():
     if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.events RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM events"))
 
