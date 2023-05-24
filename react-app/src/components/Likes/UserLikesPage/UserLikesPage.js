565ea560aabe960c06f4fdc2eecff6b4e430075f
Miketuazon <michael.d.tuazon@gmail.com>
2023-05-24 12:09:38 -0400

    got liked events to render

--- react-app/src/components/Likes/UserLikesPage/UserLikesPage.js
+++ react-app/src/components/Likes/UserLikesPage/UserLikesPage.js
@@ -10,7 +10,9 @@ const UserLikesPage = () => {
     const currentUser = useSelector(state => state?.session?.user)
     const eventsObj = useSelector(state => state.events)
     const events = Object.values(eventsObj)
-
+    console.log("events => ", events)
+    const userLikedEvents = currentUser.events_liked
+    console.log("userLikedEvents => ", userLikedEvents)
     useEffect(() => {
         dispatch(getEventsThunk())
     }, [dispatch])
@@ -18,7 +20,23 @@ const UserLikesPage = () => {
     if (!currentUser) return <h1>Unauthorized. You are not logged in</h1>
     if (!events) return <h1>Loading...</h1>
     return (
-        <h1>We are in likes</h1>
+        <div className="likes-page">
+            <h1>Likes</h1>
+            <ul className="likes-container">
+                {
+                    events.filter(event => userLikedEvents.includes(event.id))
+                        .map(event => (
+                            <div key={event.id} className="event-liked">
+                                <img className="preview-image-events" src={event.event_preview_image} alt="https://djmag.com/sites/default/files/styles/djm_23_961x540_jpg/public/article/image/EDC%20Vegas%20-%20DJ%20MAG.png.jpg"></img>
+                                <div className="event-id-and-name">{event.event_name}</div>
+                                <div className="date" style={{ fontWeight: "bold" }}>{new Date(event.event_start_date).toLocaleDateString()}</div>
+                                <div className="location">{event.event_city}, {event.event_state}</div>
+                                <div className="owner">Organizer: {event.owner.username}</div>
+                            </div>
+                        ))
+                }
+            </ul>
+        </div>
     )
 }
 
