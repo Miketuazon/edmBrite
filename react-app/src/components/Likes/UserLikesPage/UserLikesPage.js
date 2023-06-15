1b4492a51c3a35a9f5638592e52a34460405e6be
Miketuazon <michael.d.tuazon@gmail.com>
2023-06-15 16:28:02 -0400

    added no likes message in likes page

--- react-app/src/components/Likes/UserLikesPage/UserLikesPage.js
+++ react-app/src/components/Likes/UserLikesPage/UserLikesPage.js
@@ -22,23 +22,30 @@ const UserLikesPage = () => {
         dispatch(getUserLikesThunk())
     }, [dispatch, userLikedEvents])
 
-    if (!currentUser) return <h1 style={{color: "red"}}>Unauthorized. You are not logged in</h1>
+    if (!currentUser) return <h1 style={{ color: "red" }}>Unauthorized. You are not logged in</h1>
     if (!userLikedEvents) return <h1>Loading...</h1>
     return (
         <div className="likes-page">
             <h1>Likes</h1>
             <ul className="likes-container">
                 {
-                    events.filter(event => likes.includes(event.id))
-                        .map(event => (
-                            <div key={event.id} className="event-liked">
-                                <img className="preview-image-events" src={event.event_preview_image} alt="https://djmag.com/sites/default/files/styles/djm_23_961x540_jpg/public/article/image/EDC%20Vegas%20-%20DJ%20MAG.png.jpg"></img>
-                                <div className="event-id-and-name">{event.event_name}</div>
-                                <div className="date" style={{ fontWeight: "bold" }}>{new Date(event.event_start_date).toLocaleDateString()}</div>
-                                <div className="location">{event.event_city}, {event.event_state}</div>
-                                <div className="owner">Organizer: {event.owner.username}</div>
-                            </div>
-                        ))
+                    likes.length > 0 ?
+
+                        events.filter(event => likes.includes(event.id))
+                            .map(event => (
+                                <div key={event.id} className="event-liked">
+                                    <img className="preview-image-events" src={event.event_preview_image} alt="https://djmag.com/sites/default/files/styles/djm_23_961x540_jpg/public/article/image/EDC%20Vegas%20-%20DJ%20MAG.png.jpg"></img>
+                                    <div className="event-id-and-name">{event.event_name}</div>
+                                    <div className="date" style={{ fontWeight: "bold" }}>{new Date(event.event_start_date).toLocaleDateString()}</div>
+                                    <div className="location">{event.event_city}, {event.event_state}</div>
+                                    <div className="owner">Organizer: {event.owner.username}</div>
+                                </div>
+                            ))
+                        :
+                        <div className="no-likes">
+                            <h1 className="no-likes-header">You currently have no likes :( </h1>
+                            <h3>Go like an event!</h3>
+                        </div>
                 }
             </ul>
         </div>
